88d5076374a441de968119a61131bce289240d2a
beedotnet <bryantwells@me.com>
2026-03-31 11:00:48 -0400

    add exhibitors

---
+++ web/site/snippets/blocks/exhibitors.php
@@ -0,0 +1,8 @@
+<section class="Block Block--exhibitors u-bodyText">
+	<header class="Block-header">
+		Exhibitors
+	</header>
+	<div class="Block-body">
+		<?= $block->text() ?>
+	</div>
+</section>
